369c6838a20533a43e186fcd58719be8f3692be3
Islam Kamal <islamkamalx1>
2022-07-01 14:42:29 +0200

    Upload files

---
+++ app.js
@@ -0,0 +1,6 @@
+const btn = document.querySelector(".nav-toggle");
+const links = document.querySelector(".links");
+
+btn.addEventListener("click",()=>{
+    links.classList.toggle("show-links");
+});
